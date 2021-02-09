5b3c8a211b45f8c74966b3e3c607ba70276e66c6
Alexis Yushin <ayushin@apexlabs.ai>
2021-02-09 15:16:07 +0300

    depdenencies version bump

--- .cr/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Cleaning up..." \
-	&& rm -rf .deploy \
-	&& echo "Packaging..." \
-	&& cr --config .cr/config.yaml package charts/* \
-	&& echo "Uploading..." \
-	&& cr --config .cr/config.yaml upload \
-	&& echo "Indexing..." \
-	&& cr --config .cr/config.yaml index https://github.com/apexlabs-ai/django-helm-charts
